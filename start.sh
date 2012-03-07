3334afdd6e66f13982bef612d634fa8130c0afb9
iem user <noc@iem.at>
2012-03-07 10:07:56 +0100

    load some more libraries

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 cd  ${0%/*}
 
-LD_LIBRARY_PATH=/home/cubemixer/src/libsndfile-cust-chunk/src/.libs pd $@
+
+LD_LIBRARY_PATH=/home/cubemixer/src/libsndfile-cust-chunk/src/.libs pd -lib iemmatrix -lib iemlib1 -lib iemlib2 -lib zexy -path ${0%/*} $@
