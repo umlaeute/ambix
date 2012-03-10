598ca85dee565c2d5c45a753bd2a19474a566b77
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2012-03-10 15:36:48 +0100

    write simple and extended files

--- libambix/utils/ambix_test.c
+++ libambix/utils/ambix_test.c
@@ -50,9 +50,9 @@ void createfile_simple(const char*path, uint32_t ambichannels, uint32_t othercha
   } else
     printf("OK\n");
 
-  printf("Frames\t: %d\n", info.frames); 
+  printf("Frames\t: %d\n", info.frames);
 
-  printf("Samplerate\t: %f\n", info.samplerate); 
+  printf("Samplerate\t: %f\n", info.samplerate);
 
   printf("Sampleformat\t: %d (", info.sampleformat);
   switch(info.sampleformat) {
@@ -74,7 +74,7 @@ void createfile_simple(const char*path, uint32_t ambichannels, uint32_t othercha
   }
   printf(")\n");
 
-  printf("Ambisonics channels\t: %d\n", info.ambichannels); 
+  printf("Ambisonics channels\t: %d\n", info.ambichannels);
   printf("Non-Ambisonics channels\t: %d\n", info.otherchannels);
 
 
@@ -98,13 +98,92 @@ void createfile_simple(const char*path, uint32_t ambichannels, uint32_t othercha
   printf("Close file '%s': ", path);
   if(AMBIX_ERR_SUCCESS!=ambix_close(ambix))
     printf("failed\n");
-  else 
+  else
     printf("OK\n");
 }
 
+void createfile_extended(const char*path, uint32_t ambichannels, uint32_t otherchannels, uint64_t frames) {
+  ambixinfo_t info;
+  ambix_t*ambix;
+  const ambixmatrix_t*matrix;
+
+  memset(&info, 0, sizeof(info));
+  info.frames=frames;
+  info.samplerate=44100;
+  info.sampleformat=AMBIX_SAMPLEFORMAT_FLOAT32;
+  info.ambixfileformat=AMBIX_EXTENDED;
+  info.ambichannels=ambichannels;
+  info.otherchannels=otherchannels;
+
+
+  printf("Open file '%s': ", path);
+  ambix=ambix_open(path, AMBIX_WRITE, &info);
+  if(!ambix) {
+    printf("failed\n");
+    return;
+  } else
+    printf("OK\n");
+
+  printf("Frames\t: %d\n", info.frames);
+
+  printf("Samplerate\t: %f\n", info.samplerate);
+
+  printf("Sampleformat\t: %d (", info.sampleformat);
+  switch(info.sampleformat) {
+  case(AMBIX_SAMPLEFORMAT_NONE): printf("NONE"); break;
+  case(AMBIX_SAMPLEFORMAT_PCM16): printf("PCM16"); break;
+  case(AMBIX_SAMPLEFORMAT_PCM24): printf("PCM24"); break;
+  case(AMBIX_SAMPLEFORMAT_PCM32): printf("PCM32"); break;
+  case(AMBIX_SAMPLEFORMAT_FLOAT32): printf("FLOAT32"); break;
+  default: printf("**unknown**");
+  }
+  printf(")\n");
+
+  printf("ambiXformat\t: %d (", info.ambixfileformat);
+  switch(info.ambixfileformat) {
+  case(AMBIX_NONE): printf("NONE"); break;
+  case(AMBIX_SIMPLE): printf("SIMPLE"); break;
+  case(AMBIX_EXTENDED): printf("EXTENDED"); break;
+  default: printf("**unknown** 0x%04X", info.ambixfileformat);
+  }
+  printf(")\n");
+
+  printf("Ambisonics channels\t: %d\n", info.ambichannels);
+  printf("Non-Ambisonics channels\t: %d\n", info.otherchannels);
+
+
+
+  /* no write some data... */
+  do {
+    uint64_t res;
+    float32_t ambidata[64*ambichannels];
+    float32_t otherdata[64*otherchannels];
+    while(frames>64) {
+      res=ambix_writef_float32(ambix, ambidata, otherdata, 64);
+      frames-=64;
+    }
+    if(frames>0)
+      res=ambix_writef_float32(ambix, ambidata, otherdata, frames);
+
+  } while(0);
+
+
+
+  printf("Close file '%s': ", path);
+  if(AMBIX_ERR_SUCCESS!=ambix_close(ambix))
+    printf("failed\n");
+  else
+    printf("OK\n");
+}
+
+
+
 int main(int argc, char**argv) {
-  if(argc>1)
+  if(argc>1) {
     createfile_simple(argv[1], 9, 3, 44100);
+    if(argc>2)
+      createfile_extended(argv[2], 9, 3, 44100);
+  }
   else {
     fprintf(stderr, "usage: %s <ambixfilename>\n", argv[0]);
   }
