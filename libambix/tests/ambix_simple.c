108399468dafe461d421db3ddac3e0f9ff57b5c1
IOhannes m zmölnig <zmoelnig@iem.at>
2012-03-13 11:54:58 +0100

    tests for ambix simple

---
+++ libambix/tests/ambix_simple.c
@@ -0,0 +1,100 @@
+/* simple - test ambix simple
+
+   Copyright © 2012 IOhannes m zmölnig <zmoelnig@iem.at>.
+         Institute of Electronic Music and Acoustics (IEM),
+         University of Music and Dramatic Arts, Graz
+
+   This file is part of libambix
+
+   libambix is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+
+   Libgcrypt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#include "common.h"
+#include <unistd.h>
+#include <string.h>
+
+
+void check_create_simple(const char*path, ambix_sampleformat_t format) {
+  ambixinfo_t info, rinfo, winfo;
+  ambix_t*ambix=NULL;
+  float32_t*orgdata,*data,*resultdata;
+  uint32_t frames=44100;
+  uint32_t channels=4;
+  float32_t periods=10;
+  uint32_t err32;
+  float32_t diff=0., eps=1e-30;
+
+  resultdata=calloc(channels*frames, sizeof(float32_t));
+  data=calloc(channels*frames, sizeof(float32_t));
+
+  memset(&winfo, 0, sizeof(winfo));
+  memset(&info, 0, sizeof(info));
+
+  info.ambixfileformat=AMBIX_SIMPLE;
+  info.ambichannels=channels;
+  info.otherchannels=0;
+  info.samplerate=44100;
+  info.sampleformat=format;
+
+  memcpy(&rinfo, &info, sizeof(info));
+
+  ambix=ambix_open(path, AMBIX_WRITE, &rinfo);
+  fail_if((NULL==ambix), __LINE__, "couldn't create ambix file '%s' for writing", path);
+
+  orgdata=data_sine(frames, channels, periods);
+  //data_print(orgdata, 100);
+
+  memcpy(data, orgdata, frames*channels*sizeof(float32_t));
+
+  fail_if((NULL==data), __LINE__, "couldn't create data %dx%d sine @ %f", frames, channels, periods);
+
+  err32=ambix_writef_float32(ambix, data, NULL, frames);
+  fail_if((err32!=frames), __LINE__, "wrote only %d frames of %d", err32, frames);
+
+  diff=data_diff(__LINE__, orgdata, data, frames*channels);
+  fail_if((diff>eps), __LINE__, "data diff %f > %f", diff, eps);
+         
+  fail_if((AMBIX_ERR_SUCCESS!=ambix_close(ambix)), __LINE__, "closing ambix file %p", ambix);
+  ambix=NULL;
+
+  ambix=ambix_open(path, AMBIX_READ, &rinfo);
+  fail_if((NULL==ambix), __LINE__, "couldn't create ambix file '%s' for reading", path);
+
+  fail_if((info.ambixfileformat!=rinfo.ambixfileformat), __LINE__, "ambixfileformat mismatch %d!=%d", info.ambixfileformat, rinfo.ambixfileformat);
+  fail_if((info.samplerate!=rinfo.samplerate), __LINE__, "samplerate mismatch %g!=%g", info.samplerate, rinfo.samplerate);
+  fail_if((info.sampleformat!=rinfo.sampleformat), __LINE__, "sampleformat mismatch %d!=%d", info.sampleformat, rinfo.sampleformat);
+  fail_if((info.ambichannels!=rinfo.ambichannels), __LINE__, "ambichannels mismatch %d!=%d", info.ambichannels, rinfo.ambichannels);
+  fail_if((info.otherchannels!=rinfo.otherchannels), __LINE__, "otherchannels mismatch %d!=%d", info.otherchannels, rinfo.otherchannels);
+
+  err32=ambix_readf_float32(ambix, resultdata, NULL, frames);
+  fail_if((err32!=frames), __LINE__, "wrote only %d frames of %d", err32, frames);
+
+  diff=data_diff(__LINE__, orgdata, resultdata, frames*channels);
+  fail_if((diff>eps), __LINE__, "data diff %f > %f", diff, eps);
+
+  fail_if((AMBIX_ERR_SUCCESS!=ambix_close(ambix)), __LINE__, "closing ambix file %p", ambix);
+  ambix=NULL;
+
+  unlink(path);
+}
+
+
+
+int main(int argc, char**argv) {
+  check_create_simple("test-simple.caf",  AMBIX_SAMPLEFORMAT_FLOAT32);
+
+  pass();
+  return 0;
+}
