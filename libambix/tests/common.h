d295452bfd13f81da42ec163929586bdf953a62c
IOhannes m zmölnig <zmoelnig@iem.at>
2012-03-13 09:16:28 +0100

    unit tests

---
+++ libambix/tests/common.h
@@ -0,0 +1,12 @@
+#ifndef TESTS_COMMON_H
+#define TESTS_COMMON_H
+
+#include <ambix/ambix.h>
+
+#include <stdlib.h>
+static void success(void) {exit(0); }
+static void fail(void) {exit(1); }
+static void skip(void) {exit(77); }
+
+#endif /* TESTS_COMMON_H */
+
