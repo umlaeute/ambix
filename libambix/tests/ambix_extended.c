5eb247f1af5cbd010a756730ccf61a194764dd2b
IOhannes m zmölnig <zmoelnig@iem.at>
2012-03-14 11:24:07 +0100

    test for chunked writing

--- libambix/tests/ambix_extended.c
+++ libambix/tests/ambix_extended.c
@@ -25,12 +25,12 @@
 #include <string.h>
 
 
-void check_create_extended(const char*path, ambix_sampleformat_t format, float32_t eps) {
+void check_create_extended(const char*path, ambix_sampleformat_t format, uint32_t chunksize, float32_t eps) {
   ambixinfo_t info, rinfo, winfo;
   ambix_t*ambix=NULL;
   float32_t*orgambidata,*ambidata,*resultambidata;
   float32_t*orgotherdata,*otherdata,*resultotherdata;
-  uint32_t frames=441000;
+  uint32_t framesize=441000;
   uint32_t ambichannels=4;
   uint32_t extrachannels=2;
   float32_t periods=20000;
@@ -38,14 +38,15 @@ void check_create_extended(const char*path, ambix_sampleformat_t format, float32
   const ambixmatrix_t*eye2=NULL;
   uint32_t err32;
   float32_t diff=0.;
+  STARTTEST();
 
-  printf("test using '%s' [%d]\n", path, (int)format);
+  printf("test using '%s' [%d] with chunks of %d and eps=%f\n", path, (int)format, (int)chunksize, eps);
 
-  resultambidata=calloc(ambichannels*frames, sizeof(float32_t));
-  ambidata=calloc(ambichannels*frames, sizeof(float32_t));
+  resultambidata=calloc(ambichannels*framesize, sizeof(float32_t));
+  ambidata=calloc(ambichannels*framesize, sizeof(float32_t));
 
-  resultotherdata=calloc(extrachannels*frames, sizeof(float32_t));
-  otherdata=calloc(extrachannels*frames, sizeof(float32_t));
+  resultotherdata=calloc(extrachannels*framesize, sizeof(float32_t));
+  otherdata=calloc(extrachannels*framesize, sizeof(float32_t));
 
   ambix_matrix_init(ambichannels, ambichannels, &eye);
   ambix_matrix_eye(&eye);
@@ -64,26 +65,44 @@ void check_create_extended(const char*path, ambix_sampleformat_t format, float32
   ambix=ambix_open(path, AMBIX_WRITE, &rinfo);
   fail_if((NULL==ambix), __LINE__, "couldn't create ambix file '%s' for writing", path);
 
-  orgambidata=data_sine(frames, ambichannels, periods);
-  orgotherdata=data_ramp(frames, extrachannels);
+  orgambidata=data_sine(framesize, ambichannels, periods);
+  orgotherdata=data_ramp(framesize, extrachannels);
   //data_print(orgdata, 100);
-  fail_if((NULL==orgambidata), __LINE__, "couldn't create ambidata %dx%d sine @ %f", frames, ambichannels, periods);
-  fail_if((NULL==orgotherdata), __LINE__, "couldn't create otherdata %dx%d sine @ %f", frames, extrachannels, periods);
+  fail_if((NULL==orgambidata), __LINE__, "couldn't create ambidata %dx%d sine @ %f", framesize, ambichannels, periods);
+  fail_if((NULL==orgotherdata), __LINE__, "couldn't create otherdata %dx%d sine @ %f", framesize, extrachannels, periods);
 
-  memcpy(ambidata, orgambidata, frames*ambichannels*sizeof(float32_t));
-  memcpy(otherdata, orgotherdata, frames*extrachannels*sizeof(float32_t));
+  memcpy(ambidata, orgambidata, framesize*ambichannels*sizeof(float32_t));
+  memcpy(otherdata, orgotherdata, framesize*extrachannels*sizeof(float32_t));
 
   fail_if((AMBIX_ERR_SUCCESS!=ambix_setAdaptorMatrix(ambix, &eye)),
           __LINE__, "failed setting adaptor matrix");
   fail_if((AMBIX_ERR_SUCCESS!=ambix_write_header(ambix)),
           __LINE__, "failed writing headers");
 
-  err32=ambix_writef_float32(ambix, ambidata, otherdata, frames);
-  fail_if((err32!=frames), __LINE__, "wrote only %d frames of %d", err32, frames);
+  if(chunksize>0) {
+    uint32_t subframe=chunksize;
+    uint32_t chunks = framesize/chunksize;
+    uint32_t framesleft=framesize;
+    uint32_t frame;
+    printf("writing %d chunks of %d frames\n", (int)chunks, (int)chunksize);
+    for(frame=0; frame<chunks; frame++) {
+      err32=ambix_writef_float32(ambix, ambidata+ambichannels*frame*chunksize, otherdata+extrachannels*frame*chunksize, chunksize);
+      fail_if((err32!=chunksize), __LINE__, "wrote only %d chunksize of %d", err32, chunksize);
+      framesleft-=chunksize;
+    }
+    subframe=framesleft;
+    printf("writing rest of %d frames\n", (int)subframe);
+    err32=ambix_writef_float32(ambix, ambidata+ambichannels*frame*chunksize, otherdata+extrachannels*frame*chunksize, subframe);
+    fail_if((err32!=subframe), __LINE__, "wrote only %d subframe of %d", err32, subframe);
 
-  diff=data_diff(__LINE__, orgambidata, ambidata, frames*ambichannels, eps);
+  } else {
+    err32=ambix_writef_float32(ambix, ambidata, otherdata, framesize);
+    fail_if((err32!=framesize), __LINE__, "wrote only %d frames of %d", err32, framesize);
+  }
+
+  diff=data_diff(__LINE__, orgambidata, ambidata, framesize*ambichannels, eps);
   fail_if((diff>eps), __LINE__, "ambidata diff %f > %f", diff, eps);
-  diff=data_diff(__LINE__, orgotherdata, otherdata, frames*extrachannels, eps);
+  diff=data_diff(__LINE__, orgotherdata, otherdata, framesize*extrachannels, eps);
   fail_if((diff>eps), __LINE__, "otherdata diff %f > %f", diff, eps);
 
   fail_if((AMBIX_ERR_SUCCESS!=ambix_close(ambix)), __LINE__, "closing ambix file %p", ambix);
@@ -107,13 +126,13 @@ void check_create_extended(const char*path, ambix_sampleformat_t format, float32
   diff=matrix_diff(__LINE__, &eye, eye2, eps);
   fail_if((diff>eps), __LINE__, "adaptormatrix diff %f > %f", diff, eps);
 
-  err32=ambix_readf_float32(ambix, resultambidata, resultotherdata, frames);
-  fail_if((err32!=frames), __LINE__, "read only %d frames of %d", err32, frames);
+  err32=ambix_readf_float32(ambix, resultambidata, resultotherdata, framesize);
+  fail_if((err32!=framesize), __LINE__, "read only %d frames of %d", err32, framesize);
 
-  diff=data_diff(__LINE__, orgambidata, resultambidata, frames*ambichannels, eps);
+  diff=data_diff(__LINE__, orgambidata, resultambidata, framesize*ambichannels, eps);
   fail_if((diff>eps), __LINE__, "ambidata diff %f > %f", diff, eps);
 
-  diff=data_diff(__LINE__, orgotherdata, resultotherdata, frames*extrachannels, eps);
+  diff=data_diff(__LINE__, orgotherdata, resultotherdata, framesize*extrachannels, eps);
   fail_if((diff>eps), __LINE__, "otherdata diff %f > %f", diff, eps);
 
   fail_if((AMBIX_ERR_SUCCESS!=ambix_close(ambix)), __LINE__, "closing ambix file %p", ambix);
@@ -123,16 +142,23 @@ void check_create_extended(const char*path, ambix_sampleformat_t format, float32
   free(ambidata);
   free(resultotherdata);
   free(otherdata);
-  //  unlink(path);
+  unlink(path);
 }
 
 
 
 int main(int argc, char**argv) {
-  check_create_extended("test2-float32.caf",  AMBIX_SAMPLEFORMAT_FLOAT32, 1e-7);
-  check_create_extended("test2-pcm32.caf",  AMBIX_SAMPLEFORMAT_PCM32, 1e-5);
-  check_create_extended("test2-pcm16.caf",  AMBIX_SAMPLEFORMAT_PCM16, 1./20000.);
+#if 1
+  check_create_extended("test2-float32.caf",AMBIX_SAMPLEFORMAT_FLOAT32, 0, 1e-7);
+  check_create_extended("test2-pcm32.caf",  AMBIX_SAMPLEFORMAT_PCM32, 0, 1e-5);
+  check_create_extended("test2-pcm16.caf",  AMBIX_SAMPLEFORMAT_PCM16, 0, 1./20000.);
+#endif
 
+#if 1
+  check_create_extended("test2-float32.caf",AMBIX_SAMPLEFORMAT_FLOAT32, 1024, 1e-7);
+  check_create_extended("test2-pcm32.caf",  AMBIX_SAMPLEFORMAT_PCM32, 1024, 1e-5);
+  check_create_extended("test2-pcm16.caf",  AMBIX_SAMPLEFORMAT_PCM16, 1024, 1./20000.);
+#endif
 
   pass();
   return 0;
