98fdc998b71e6f37b60de8229e157240941c2033
IOhannes m zmölnig <zmoelnig@iem.at>
2012-03-21 13:57:26 +0100

    added ambix-dump, as simple utility that dumps ambix-files to the stdout

---
+++ utils/ambix-dump.c
@@ -0,0 +1,399 @@
+/* ambix_dump -  create an ambix file              -*- c -*-
+
+   Copyright © 2012 IOhannes m zmölnig <zmoelnig@iem.at>.
+         Institute of Electronic Music and Acoustics (IEM),
+         University of Music and Dramatic Arts, Graz
+
+   This file is part of libambix
+
+   libambix is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+
+   Libgcrypt is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+/**
+ * @brief ambix_dump - merge several (multi-channel) audio files into a single ambix file
+ *
+ * ambix_dump -o <outfile> [-O <order>] [-X <matrixfile>] <infile1> [<infile2> ...]
+ * merge several (multi-channel) audio files into a single ambix file;
+ * infile1 becomes W-channel, infile2 becomes X-channel,...
+ * by default this will write an 'ambix simple' file (only full sets are accepted)
+ * eventually files are written as 'ambix extended' file with adaptor matrix set to unity
+ * if 'order' is specified, all inchannels not needed for the full set are written as 'extrachannels'
+ * 'matrixfile' is a soundfile/octavefile that is interpreted as matrix: each channel is a row, sampleframes are columns
+ * if 'matrix' is specified it must construct a full-set (it must satisfy rows=(O+1)^2)
+ * if 'matrix' is specified, all inchannels not needed to reconstruct to a full set are 'extrachannels'
+ * if both 'order' and 'matrix' are specified they must match
+ */
+
+#ifdef HAVE_CONFIG_H
+# include "config.h"
+#endif /* HAVE_CONFIG_H */
+
+#include "ambix/ambix.h"
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define AMBIX_DUMP_VERSION "0.1"
+
+#define MARK() printf("%s:%d[%s]\n", __FILE__, __LINE__, __FUNCTION__)
+
+typedef struct ai_t {
+  ambix_t*ambix;
+  ambix_info_t info;
+
+  char*filename;
+
+  const ambix_matrix_t*matrix;
+
+  ambix_fileformat_t format;
+
+  int dumpRaw;
+  int dumpCooked;
+  int dumpXtra;
+
+  uint32_t blocksize;
+#define DEFAULT_BLOCKSIZE 1024
+#define MAX_FILENAMESIZE 1024
+} ai_t;
+static void print_usage(const char*path);
+static void print_version(const char*path);
+
+static ai_t*ai_cmdline(const char*name, int argc, char**argv) {
+  ai_t*ai=calloc(1, sizeof(ai_t));
+  uint32_t blocksize=0;
+  int dumpRaw=0, dumpCooked=0, dumpXtra=0;
+  ambix_fileformat_t format=AMBIX_EXTENDED;
+
+  while(argc) {
+    if(!strcmp(argv[0], "-h") || !strcmp(argv[0], "--help")) {
+      print_usage(name);
+      exit(0);
+    }
+    if(!strcmp(argv[0], "-v") || !strcmp(argv[0], "--version")) {
+      print_version(name);
+      exit(0);
+    }
+    if(!strcmp(argv[0], "-r") || !strcmp(argv[0], "--raw")) {
+      dumpRaw=1;
+      argv++; argc--;
+      continue;
+    }
+    if(!strcmp(argv[0], "-a") || !strcmp(argv[0], "--ambisonics")) {
+      dumpCooked=1;
+      argv++; argc--;
+      continue;
+    }
+    if(!strcmp(argv[0], "-x") || !strcmp(argv[0], "--extra")) {
+      dumpXtra=1;
+      argv++; argc--;
+      continue;
+    }
+    if(!strcmp(argv[0], "-S") || !strcmp(argv[0], "--simple")) {
+      format=AMBIX_SIMPLE;
+      argv++; argc--;
+      continue;
+    }
+    if(!strcmp(argv[0], "-X") || !strcmp(argv[0], "--extended")) {
+      format=AMBIX_EXTENDED;
+      argv++; argc--;
+      continue;
+    }
+    if(!strcmp(argv[0], "-b") || !strcmp(argv[0], "--blocksize")) {
+      if(argc>1) {
+        blocksize=atoi(argv[1]);
+        argv+=2;
+        argc-=2;
+        continue;
+      }
+      return NULL;
+    }
+    if(argc) {
+      ai->filename=strdup(argv[0]);
+    }
+    break;
+  }
+
+  if(!ai->filename)
+    return NULL;
+
+  if(blocksize>0)
+    ai->blocksize=blocksize;
+  else
+    ai->blocksize=DEFAULT_BLOCKSIZE;
+
+
+  if(!dumpRaw && !dumpCooked && !dumpXtra) {
+    dumpCooked=1;
+  }
+
+  if(format)
+    ai->format = format;
+  else
+    ai->format = AMBIX_EXTENDED;
+
+  if(format == AMBIX_SIMPLE)
+    dumpRaw=dumpRaw || dumpCooked;
+
+  ai->dumpRaw=dumpRaw;
+  ai->dumpCooked=dumpCooked;
+  ai->dumpXtra=dumpXtra;
+
+
+  return ai;
+}
+
+static ai_t*ai_close(ai_t*ai) {
+  uint32_t i;
+  if(!ai)return NULL;
+
+  //  printf("closing %d outhandles %p\n", ai->numOuts, ai->outhandles);
+  if(ai->ambix) {
+    ambix_close(ai->ambix);
+  }
+  ai->ambix=NULL;
+
+  return NULL;
+}
+
+static ai_t*ai_open_input(ai_t*ai) {
+  uint32_t i;
+  uint32_t channels=0;
+  const ambix_matrix_t*matrix=NULL;
+  if(!ai)return ai;
+  if(!ai->ambix) {
+    ai->info.fileformat=ai->format;
+
+    ai->ambix=ambix_open(ai->filename, AMBIX_READ, &ai->info);
+  }
+  if(!ai->ambix) {
+    return ai_close(ai);
+  }
+
+  if((ai->info.ambichannels < 1) && (ai->info.extrachannels < 1)) {
+    return ai_close(ai);
+  }
+
+  ai->matrix=ambix_get_adaptormatrix(ai->ambix);
+
+  return ai;
+}
+
+static void printf_block(const char*prefix, float*data, uint32_t channels, uint32_t frames) {
+  printf("%s:*[%dx%d]@%p\n", prefix, frames, channels, data);
+
+  if(data) {
+    uint32_t c, f;
+    for(f=0; f<frames; f++) {
+      printf("%s[%05d]:", prefix, f);
+      for(c=0; c<channels; c++) {
+        printf(" %05g", *data);
+        data++;
+      }
+      printf("\n");
+    }
+  }
+}
+static ai_t*ai_dump_block(ai_t*ai,
+                          float*rawdata,
+                          float*cookeddata,
+                          float*extradata,
+                          float*dumpbuffer,
+                          uint64_t frames) {
+  uint32_t i;
+  uint32_t ambichannels, fullambichannels, extrachannels;
+  uint64_t channel, f, c, channels=0;
+
+  const ambix_matrix_t*matrix;
+  float*source, *dest;
+
+  //printf("rawdata=%p\tcookeddata=%p\textradata=%p\n", rawdata, cookeddata, extradata);
+
+  if(!ai)return ai;
+  ambichannels=ai->info.ambichannels;
+  extrachannels=ai->info.extrachannels;
+
+
+
+  matrix=ai->matrix;
+  if(matrix) {
+    uint32_t rows=matrix->rows;
+    uint32_t cols=matrix->cols;
+
+    fullambichannels=rows;
+    if(cols!=ambichannels) {
+      printf("columns do not match ambichannels %d!=%d\n", cols, ambichannels);
+    }
+  } else {
+    fullambichannels=ambichannels;
+  }
+
+  /* read the raw data */
+  if(frames!=ambix_readf_float32(ai->ambix,
+                                 rawdata,
+                                 extradata,
+                                 frames)) {
+    return ai_close(ai);
+  }
+
+  if(ai->dumpRaw) {
+    printf_block("RAW", rawdata, ambichannels, frames);
+  }
+
+  /* decode the ambisonics data */
+  //  printf("reading ambidata %p & %p\n", rawdata, cookeddata);
+  if(rawdata && cookeddata && matrix) {
+    source=rawdata;
+    dest  =cookeddata;
+
+    if(AMBIX_ERR_SUCCESS!=ambix_matrix_multiply_float32(cookeddata, matrix, rawdata, frames)) {
+      printf("failed decoding\n");
+      return ai_close(ai);
+    }
+  }
+  if(ai->dumpCooked) {
+    printf_block("AMB", cookeddata, fullambichannels, frames);
+  }
+
+  /* dump the extra data */
+  //  printf("reading extradata %p\n", extradata);
+  if(ai->dumpXtra) {
+    printf_block("XTR", extradata, extrachannels, frames);
+  }
+
+  //printf("reading done\n");
+  return ai;
+}
+
+static ai_t*ai_dodump(ai_t*ai) {
+  uint64_t blocksize=0, blocks=0;
+  uint64_t f, frames=0, channels=0;
+  float32_t*rawdata=NULL, *cookeddata=NULL, *extradata=NULL,*dumpbuf=NULL;
+  uint64_t size=0;
+  if(!ai)return ai;
+
+  blocksize=ai->blocksize;
+  if(blocksize<1)
+    blocksize=DEFAULT_BLOCKSIZE;
+  frames=ai->info.frames;
+  channels=(ai->info.ambichannels+ai->info.extrachannels);
+
+  if(ai->info.ambichannels) {
+    const ambix_matrix_t*matrix=ai->matrix;
+    if(!matrix) {
+      printf("no adaptor matrix founde...\n");
+      //      return ai_close(ai);
+    }
+    size=(ai->info.ambichannels)*blocksize;
+    rawdata=malloc(sizeof(float32_t)*size);
+    if(matrix) {
+      cookeddata=malloc(sizeof(float32_t)*(matrix->rows)*blocksize);
+      if((matrix->rows)*blocksize > size)
+        size=(matrix->rows)*blocksize;
+    }
+
+  }
+
+  if(ai->info.extrachannels) {
+    extradata=malloc(sizeof(float32_t)*(ai->info.extrachannels)*blocksize);
+    if((ai->info.extrachannels)*blocksize > size)
+      size=(ai->info.extrachannels)*blocksize;
+  }
+  dumpbuf=malloc(sizeof(float32_t)*size);
+
+  while(frames>blocksize) {
+    blocks++;
+    if(!ai_dump_block(ai, rawdata, cookeddata, extradata, dumpbuf, blocksize)) {
+      return ai_close(ai);
+    }
+    frames-=blocksize;
+  }
+  if(!ai_dump_block(ai, rawdata, cookeddata, extradata, dumpbuf, frames)) {
+    return ai_close(ai);
+  }
+
+  free(rawdata);
+  free(cookeddata);
+  free(extradata);
+  free(dumpbuf);
+
+  //  printf("reading really done %p\n", ai);
+  return ai;
+}
+
+
+
+static int ambix_dump(ai_t*ai) {
+  ai_t*result=ai_open_input(ai);
+  //  if(result)printf("success @ %d!\n", __LINE__);
+  result=ai_dodump(result);
+  //if(result)printf("success @ %d!\n", __LINE__);
+
+  ai_close(ai);
+  //if(result)printf("success @ %d!\n", __LINE__);
+  //  printf("dump done %p\n", result);
+  return (result!=NULL);
+}
+
+
+
+
+int main(int argc, char**argv) {
+  ai_t*ai=ai_cmdline(argv[0], argc-1, argv+1);
+  if(!ai) {
+    print_usage(argv[0]);//"ambix_dump");
+    return 1;
+  }
+
+  return ambix_dump(ai);
+}
+void print_usage(const char*name) {
+  printf("\n");
+  printf("Usage: %s [options] infile\n", name);
+  printf("Split an ambix file into several mono files\n");
+
+  printf("\n");
+  printf("Options:\n");
+  printf("  -h, --help                       print this help\n");
+  printf("  -v, --version                    print version info\n");
+  printf("  -r, --raw                        dump raw ambisonics data\n");
+  printf("  -a, --ambisonics                 dump cooked ambisonics data\n");
+  printf("  -x, --extra                      dump additional audio data\n");
+
+  printf("  -S, --simple                      open as AMBIX_SIMPLE\n");
+  printf("  -X, --extended                    open as AMBIX_EXTENDED\n");
+
+  printf("\n");
+
+  printf(
+         "\nDumps the contents of an ambix file to the stderr in human readable form:"
+         "\neach frame is output in a separate line, with the channels separated by space"
+         "\n"
+         );
+
+  printf("Report bugs to: zmoelnig@iem.at\n\n");
+  printf("Home page: http://ambisonics.iem.at/xchange/products/libambix\n", name);
+}
+void print_version(const char*name) {
+  printf("%s %s\n", name, AMBIX_DUMP_VERSION);
+  printf("\n");
+  printf("Copyright (C) 2012 Institute of Electronic Music and Acoustics (IEM), University of Music and Dramatic Arts (KUG), Graz, Austria.\n");
+  printf("\n");
+  printf("License LGPLv2.1: GNU Lesser GPL version 2.1 or later <http://gnu.org/licenses/lgpl.html>\n");
+  printf("This is free software: you are free to change and redistribute it.\n");
+  printf("There is NO WARRANTY, to the extent permitted by law.\n");
+  printf("\n");
+  printf("Written by IOhannes m zmoelnig <zmoelnig@iem.at>\n");
+}
