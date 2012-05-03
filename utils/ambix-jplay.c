e727bdb03353fae2e343c331518c3cd7824a1ff3
IOhannes m zmölnig <zmoelnig@iem.at>
2012-05-03 15:36:09 +0200

    added copyright boilerplate to ambix-jplay

--- utils/ambix-jplay.c
+++ utils/ambix-jplay.c
@@ -1,4 +1,27 @@
-/***** jack.play.c - (c) rohan drape, 2003-2010 *****/
+/* ambix-jplay.c -  play back an ambix file via jack            -*- c -*-
+
+   Copyright © 2003-2001 Rohan Drape
+   Copyright © 2012 IOhannes m zmölnig <zmoelnig@iem.at>.
+         Institute of Electronic Music and Acoustics (IEM),
+         University of Music and Dramatic Arts, Graz
+
+   This file is based on 'jack.play' from Rohan Drape's "jack-tools" collection
+
+   you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as
+   published by the Free Software Foundation; either version 2 of
+   the License, or (at your option) any later version.
+
+   ambix-jplay is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 
 #define HAVE_SAMPLERATE
 //#define HAVE_AMBIX_SEEK
