0bda0df56503415de6dba1c8a305e9c830b5e4d4
IOhannes m zmölnig <zmoelnig@iem.at>
2012-05-15 18:35:02 +0200

    changed 'Libgcrypt' to 'libambix' in boilerplate

--- libambix/src/adaptor.c
+++ libambix/src/adaptor.c
@@ -11,7 +11,7 @@
    published by the Free Software Foundation; either version 2.1 of
    the License, or (at your option) any later version.
 
-   Libgcrypt is distributed in the hope that it will be useful,
+   libambix is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU Lesser General Public License for more details.
--- pd/ambix_read~.c
+++ pd/ambix_read~.c
@@ -1,23 +1,23 @@
 /* ambix_read~ -  read AMBIsonics Xchange files in Pd              -*- c -*-
 
-Copyright © 2012 IOhannes m zmölnig <zmoelnig@iem.at>.
-Institute of Electronic Music and Acoustics (IEM),
-University of Music and Dramatic Arts, Graz
+ Copyright © 2012 IOhannes m zmölnig <zmoelnig@iem.at>.
+ Institute of Electronic Music and Acoustics (IEM),
+ University of Music and Dramatic Arts, Graz
 
-This file is part of libambix
+ This file is part of libambix
 
-libambix is free software; you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as
-published by the Free Software Foundation; either version 2.1 of
-the License, or (at your option) any later version.
+ libambix is free software; you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as
+ published by the Free Software Foundation; either version 2.1 of
+ the License, or (at your option) any later version.
 
-Libgcrypt is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+ libambix is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public
-License along with this program; if not, see <http://www.gnu.org/licenses/>.
+ You should have received a copy of the GNU Lesser General Public
+ License along with this program; if not, see <http://www.gnu.org/licenses/>.
 
 */
 
