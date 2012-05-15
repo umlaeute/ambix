d0c829813db22eb4f934248df0079509587d1898
IOhannes m zmölnig <zmoelnig@iem.at>
2012-05-15 14:07:16 +0200

    more cross-references

--- libambix/ambix/ambix.h
+++ libambix/ambix/ambix.h
@@ -47,23 +47,23 @@ extern "C" {
  *
  * @param path filename of the file to open
  *
- * @param mode whether to open the file for reading and/or writing (AMBIX_READ,
- * AMBIX_WRITE, AMBIX_READ | AMBIX_WRITE)
+ * @param mode whether to open the file for reading and/or writing (@ref AMBIX_READ,
+ * @ref AMBIX_WRITE, @ref AMBIX_RDRW)
  *
  * @param ambixinfo pointer to a valid ambix_info_t structure
  *
  * @remark when opening a file for reading, the structure should be initialized
  * to zero before calling ambix_open(): the fields will be set by the library;
- * if you set the ambixinfo.ambixformat field to something else than AMBIX_NONE,
+ * if you set the ambixinfo.ambixformat field to something else than @ref AMBIX_NONE,
  * the library will present the data as if the was written in this format (e.g.
- * if you set ambixinfo.ambixformat to AMBIX_BASIC but the file really is
- * AMBIX_EXTENDED, the library will automatically pre-multiply the
+ * if you set ambixinfo.ambixformat to @ref AMBIX_BASIC but the file really is
+ * @ref AMBIX_EXTENDED, the library will automatically pre-multiply the
  * reconstruction matrix to give you the full ambisonics set.
  *
  * @remark when opening a file for writing, the caller must set the fields; if
- * ambixinfo.ambixformat is AMBIX_NONE, than ambixinfo.ambichannels must be 0,
+ * ambixinfo.ambixformat is @ref AMBIX_NONE, than ambixinfo.ambichannels must be 0,
  * else ambixinfo.ambichannels must be >0; if ambixinfo.ambixformat is
- * AMBIX_BASIC, then ambixinfo.ambichannels must be @f$(order_{ambi}+1)^2@f$
+ * @ref AMBIX_BASIC, then ambixinfo.ambichannels must be @f$(order_{ambi}+1)^2@f$
  *
  * @return A handle to the opened file (or NULL on failure)
  */
@@ -198,7 +198,7 @@ SNDFILE* ambix_get_sndfile (ambix_t* ambix);
  *
  * The ambix extended fileformat comes with a adaptor matrix, that can be used
  * to reconstruct a full 3d ambisonics set from the channels stored in ambix
- * file. In the ambix BASIC format no adaptor matrix is present, the file
+ * file. In the ambix @ref AMBIX_BASIC format no adaptor matrix is present, the file
  * always contains the full set.
  *
  * @remark the adaptor matrix can only be obtained for a ambix extended file; if
@@ -221,14 +221,15 @@ const ambix_matrix_t* ambix_get_adaptormatrix (ambix_t* ambix);
 /** @brief Set a matrix to be pre-multiplied
  *
  * Adds an (additional) adaptor matrix to the processing. Depending on the mode
- * of operation this can have different meanings! When READing an ambix 'BASIC'
- * file, this tells the library to do an (additional) matrix-multiplication When
- * reconstructing the full ambisonics set; you can use use this to get the
- * ambisonics channels in a format other than SN3D/ACN (e.g. using an ambix to
- * Furse-Malham adaptor matrix) or getting the loudspeaker feeds directly (by
- * supplying a decoder matrix); in this case, the matrix MUST have
- * ambix->ambichannels columns. When WRITEing an ambix 'EXTENDED' file, this
- * tells the library to store the matrix as the adaptor matrix within the file.
+ * of operation this can have different meanings! When READing an ambix '@ref
+ * AMBIX_BASIC' file, this tells the library to do an (additional)
+ * matrix-multiplication When reconstructing the full ambisonics set; you can
+ * use use this to get the ambisonics channels in a format other than SN3D/ACN
+ * (e.g. using an ambix to Furse-Malham adaptor matrix) or getting the
+ * loudspeaker feeds directly (by supplying a decoder matrix); in this case, the
+ * matrix MUST have ambix->ambichannels columns. When WRITEing an ambix '@ref
+ * AMBIX_EXTENDED' file, this tells the library to store the matrix as the
+ * adaptor matrix within the file.
  *
  * @param ambix The handle to an ambix file
  *
@@ -237,7 +238,8 @@ const ambix_matrix_t* ambix_get_adaptormatrix (ambix_t* ambix);
  *
  * @return an errorcode indicating success
  *
- * @remark using this on ambix handles other than READ/BASIC or WRITE/EXTENDED
+ * @remark using this on ambix handles other than @ref AMBIX_READ/@ref
+ * AMBIX_BASIC or @ref AMBIX_WRITE/@ref AMBIX_EXTENDED
  * is an error.
  */
 AMBIX_API
