c05a259a8de95d49f9cc27e17be419aad7af8ced
IOhannes m zmölnig <zmoelnig@iem.at>
2012-06-06 11:34:38 +0200

    added hacks to ease compilation with MSVC/MinGW

---
+++ pd/winhacks.c
@@ -0,0 +1,52 @@
+/* winhacks.h -  AMBIsonics eXchange hacks for w32 compilation     -*- c -*-
+
+   Copyright © 2012 IOhannes m zmölnig <zmoelnig@iem.at>.
+         Institute of Electronic Music and Acoustics (IEM),
+         University of Music and Dramatic Arts, Graz
+
+   Copyright © 2005 Free Software Foundation, Inc.
+         Written by Kaveh R. Ghazi <ghazi@caip.rutgers.edu>.
+
+   This file is part of libambix
+
+   libambix is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+
+   libambix is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifdef __MINGW32__
+int _get_output_format( void ) { 
+	return 1; 
+}
+#endif
+
+#ifdef _MSC_VER
+#include <string.h>
+#include <stdlib.h>
+char *
+strndup (const char *s, size_t n)
+{
+  char *result = NULL;
+  size_t len = strlen (s);
+
+  if (n < len)
+    len = n;
+
+  result = (char *) malloc (len + 1);
+  if (!result)
+    return 0;
+
+  result[len] = '\0';
+  return (char *) memcpy (result, s, len);
+}
+#endif
---
+++ pd/winhacks.h
@@ -0,0 +1,35 @@
+/* winhacks.h -  AMBIsonics eXchange hacks for w32 compilation     -*- c -*-
+
+   Copyright © 2012 IOhannes m zmölnig <zmoelnig@iem.at>.
+         Institute of Electronic Music and Acoustics (IEM),
+         University of Music and Dramatic Arts, Graz
+
+   This file is part of libambix
+
+   libambix is free software; you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as
+   published by the Free Software Foundation; either version 2.1 of
+   the License, or (at your option) any later version.
+
+   libambix is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this program; if not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef WINHACKS_H
+#define WINHACKS_H
+
+#ifdef __MINGW32__
+int _get_output_format( void );
+#endif
+#ifdef _MSC_VER
+char *strndup (const char *s, size_t n);
+#endif
+
+
+#endif WINHACKS_H
