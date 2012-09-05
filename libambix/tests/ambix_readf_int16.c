112fca70efee7c2b2854174026ac9a3de960c8e5
ivana <imiserda@yahoo.com>
2012-09-05 15:16:26 +0200

    Added new test for read and write functions, don't work

---
+++ libambix/tests/ambix_readf_int16.c
@@ -0,0 +1,7 @@
+#include "common.h"
+
+int main ()
+{
+  pass();
+  return 0;
+}
---
+++ libambix/tests/ambix_writef_int16.c
@@ -0,0 +1,27 @@
+#include "common.h"
+
+int main ()
+{
+  ambix_t *ambix = NULL;
+  ambix_info_t *info= calloc(1, sizeof(ambix_info_t));
+  int16_t *ambidata= 0;
+  int16_t *otherdata=0;
+  int16_t frames= 10;
+  
+  
+  ambix=ambix_open("test.caf", AMBIX_READ, info);
+  int16_t result=0;
+  
+  /* ambix_writef_int16 (ambix_t *ambix, const int16_t *ambidata, const int16_t *otherdata, int64_t frames)
+     Write (16bit signed integer) samples to the ambix file. 
+  Returns:
+    the number of sample frames successfully written 
+    */
+  
+  // UNCOMMENT WHEN FIXED
+  /*result=ambix_writef_int16 (ambix,ambidata,otherdata,frames);
+  fail_if (frames!=result, __LINE__, "Files were not wrotten successfully");
+  */
+  
+  return 0;
+}
