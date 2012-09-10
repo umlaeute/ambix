90302590122850c861e7d6873bb77a6b89233576
IOhannes m zmölnig <zmoelnig@iem.at>
2012-09-10 10:52:39 +0200

    skip test if file cannot be opened rather than fail

--- libambix/tests/ambix_seek.c
+++ libambix/tests/ambix_seek.c
@@ -2,13 +2,14 @@
 
 int main()
 {
+  int result=0;
   
   ambix_t *ambix = NULL;
   ambix_info_t *info= calloc(1, sizeof(ambix_info_t));
-  ambix=ambix_open(AMBIXTEST_FILE1, AMBIX_READ, info);
-  
-  int result=0;
 
+  ambix=ambix_open(AMBIXTEST_FILE1, AMBIX_READ, info);
+  skip_if(NULL==ambix, __LINE__, "ambix_open(%s, AMBIX_READ, info)", AMBIXTEST_FILE1);
+  
   result= ambix_seek (ambix, 100, SEEK_CUR);
   fail_if (result!=100, __LINE__, "File failed to seek correct data");
   
