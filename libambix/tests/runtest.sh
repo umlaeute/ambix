c963b0282e19d6467259c1419c54a966b04bd5a4
IOhannes m zmölnig <zmoelnig@iem.at>
2012-09-10 12:19:38 +0200

    wrapper scripts to run tests in valgrind

---
+++ libambix/tests/runtest.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+APP=$1
+
+_TESTTYPE=$2
+TESTTYPE=${TESTTYPE:-$_TESTTYPE}
+
+##################################
+
+VALGRIND="libtool --mode=execute valgrind --error-exitcode=1 -q"
+
+if which valgrind > /dev/null
+then
+ MEMGRIND="${VALGRIND} --leak-check=full"
+ DRDGRIND="${VALGRIND} --tool=drd"
+ HELGRIND="${VALGRIND} --tool=helgrind"
+else
+ MEMGRIND=
+ DRDGRIND=
+ HELGRIND=
+fi
+
+get_runtest() {
+case "$1" in
+ mem*|MEM*)
+    echo ${MEMGRIND}
+ ;;
+ DRD|drd)
+    echo ${DRDGRIND}
+ ;;
+ HEL*|hel*)
+    echo ${HELGRIND}
+ ;;
+ *)
+    
+ ;;
+esac
+}
+
+
+$(get_runtest $TESTTYPE) ${APP}
