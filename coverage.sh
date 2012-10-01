cf3a362981c35ae9f53858e662e4f25f9acb4b22
IOhannes m zmölnig <zmoelnig@iem.at>
2012-10-01 20:34:34 +0200

    code-coverage analyser using gcov/lcov

---
+++ coverage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+INFOFILE="libambix.info"
+OUTDIR=coverage/
+MAKE=make
+
+${MAKE} clean
+
+# Reconfigure with gcov support
+CXXFLAGS="-g -O0 --coverage" CFLAGS="-g -O0 --coverage" ./configure --disable-shared --without-rte || exit 1
+
+# Generate gcov output
+${MAKE} || exit 1
+
+# Generate html report
+
+lcov --base-directory . --directory . --zerocounters -q || exit 1
+${MAKE} check || exit 1
+lcov --base-directory libambix/src --directory libambix/src -c -o ${INFOFILE} || exit 1
+lcov --base-directory libambix/tests --directory libambix/tests -c -o tests_${INFOFILE} || exit 1
+
+# remove output for external libraries
+lcov --remove ${INFOFILE} "/usr*" -o ${INFOFILE}  || exit 1
+lcov --remove tests_${INFOFILE} "/usr*" -o tests_${INFOFILE}  || exit 1
+
+rm -rf "${OUTDIR}"
+genhtml -o "${OUTDIR}" -t "libambix test coverage" --num-spaces 4 ${INFOFILE} tests_${INFOFILE}
