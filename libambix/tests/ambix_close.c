21ac7c586db3199a941d89804dc1f4c3dd93dffd
IOhannes m zmoelnig <zmoelnig@umlautO.umlaeute.mur.at>
2013-09-25 16:19:39 +0200

    ambix_close() returns ambix_err_t
    and not a pointer

--- libambix/tests/ambix_close.c
+++ libambix/tests/ambix_close.c
@@ -9,7 +9,7 @@ int main()
   ambix=ambix_open(AMBIXTEST_FILE1, AMBIX_READ, info);
   skip_if (NULL==ambix, __LINE__, "ambix_open(%s, AMBIX_READ, info)", AMBIXTEST_FILE1);
   
-  fail_if (ambix_close(ambix)!=NULL, __LINE__, "File was not closed properly");
+  fail_if (ambix_close(ambix)!=AMBIX_ERR_SUCCESS, __LINE__, "File was not closed properly");
     
   free(info);
   return 0;
