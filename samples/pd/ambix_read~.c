fcd9fe9fa4da29d70e821d352543d3e33195170b
IOhannes m zmölnig <zmoelnig@iem.at>
2014-04-14 11:24:11 +0200

    (un)locking mutex in ctor
    this is a bit of a dummy operation, as it (un)locks the mutex before
    the thread is even created.
    it's mainly here to satisfy coverity.
    
    CID:22427
    CID:22428
    CID:22429
    CID:22430

--- samples/pd/ambix_read~.c
+++ samples/pd/ambix_read~.c
@@ -506,17 +506,22 @@ static void *ambix_read_new(t_symbol*s, int argc, t_atom*argv) {
   x->x_ambichannels = achannels;
   x->x_xtrachannels = xchannels;
 
+  x->x_canvas = canvas_getcurrent();
+
   pthread_mutex_init(&x->x_mutex, 0);
   pthread_cond_init(&x->x_requestcondition, 0);
   pthread_cond_init(&x->x_answercondition, 0);
+
+  pthread_mutex_lock(&x->x_mutex);
   x->x_vecsize = DEFAULTVECSIZE;
   x->x_state = STATE_IDLE;
   x->x_clock = clock_new(x, (t_method)ambix_read_tick);
-  x->x_canvas = canvas_getcurrent();
   x->x_buf = buf;
   x->x_bufsize = bufsize;
   x->x_bufframes = bufframes;
   x->x_fifosize = x->x_fifohead = x->x_fifotail = x->x_requestcode = 0;
+  pthread_mutex_lock(&x->x_mutex);
+
   pthread_create(&x->x_childthread, 0, ambix_read_child_main, x);
   return (x);
 }
