0ca1718e9f17754a47700016b1f438c35bba5d0d
IOhannes m zmölnig <zmoelnig@iem.at>
2016-06-21 15:44:29 +0200

    adapted to new type-agnostic API

--- libambix/tests/common_basic2extended.c
+++ libambix/tests/common_basic2extended.c
@@ -28,7 +28,7 @@ uint32_t max_u32(uint32_t a, uint32_t b) { return (a>b)?a:b;}
 
 int check_create_b2e(const char*path, ambix_sampleformat_t format,
 		      ambix_matrix_t*matrix, uint32_t extrachannels,
-		      uint32_t chunksize, float32_t eps) {
+		      uint32_t chunksize, ambixtest_presentationformat_t fmt,float32_t eps) {
   uint32_t fullambichannels = matrix?matrix->rows:0;
   uint32_t ambixchannels = matrix?matrix->cols:0;
   ambix_info_t info, rinfo, winfo;
@@ -43,8 +43,6 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
   ambix_err_t err=0;
   STARTTEST("ambi=[%dx%d],extra=%d, format=%d\n",
 	    matrix?matrix->rows:-1, matrix?matrix->cols:-1, extrachannels, format);
-
-  //printf("test using '%s' [%d] with chunks of %d and eps=%f\n", path, (int)format, (int)chunksize, eps);
 
   resultambidata=(float32_t*)calloc(max_u32(fullambichannels, ambixchannels)*framesize, sizeof(float32_t));
   ambidata=(float32_t*)calloc(fullambichannels*framesize, sizeof(float32_t));
@@ -66,9 +64,8 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
   ambix=ambix_open(path, AMBIX_WRITE, &winfo);
   if(fail_if((NULL==ambix), __LINE__, "couldn't create ambix file '%s' for writing", path))return 1;
 
-  orgambidata=data_sine(framesize, fullambichannels, periods);
-  orgotherdata=data_ramp(framesize, extrachannels);
-  //data_print(orgdata, 100);
+  orgambidata =data_sine(fmt, framesize, fullambichannels, periods);
+  orgotherdata=data_ramp(fmt, framesize, extrachannels);
   if(fail_if((NULL==orgambidata), __LINE__, "couldn't create ambidata %dx%d sine @ %f", (int)framesize, (int)fullambichannels, (float)periods))return 1;
   if(fail_if((NULL==orgotherdata), __LINE__, "couldn't create otherdata %dx%d sine @ %f", (int)framesize, (int)extrachannels, (float)periods))return 1;
 
@@ -76,6 +73,7 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
   memcpy(otherdata, orgotherdata, framesize*extrachannels*sizeof(float32_t));
 
   err=ambix_set_adaptormatrix(ambix, matrix);
+#if 0
   if(1 || AMBIX_ERR_SUCCESS!=err) {
     ambix_matrix_t*pinv=0;
     //printf("adaptor matrix:\n"); matrix_print(matrix);
@@ -88,6 +86,7 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
     }
     if(pinv)ambix_matrix_destroy(pinv);
   }
+#endif
   if(fail_if((AMBIX_ERR_SUCCESS!=err), __LINE__, "failed setting adaptor matrix [%d]", err))return 1;
 
   if(chunksize>0) {
@@ -95,14 +94,12 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
     uint32_t chunks = framesize/chunksize;
     uint32_t framesleft=framesize;
     uint32_t frame;
-    //printf("writing %d chunks of %d frames\n", (int)chunks, (int)chunksize);
     for(frame=0; frame<chunks; frame++) {
       err64=ambix_writef_float32(ambix, ambidata+fullambichannels*frame*chunksize, otherdata+extrachannels*frame*chunksize, chunksize);
       if(fail_if((err64!=chunksize), __LINE__, "wrote only %d chunksize of %d", (int)err64, (int)chunksize))return 1;
       framesleft-=chunksize;
     }
     subframe=framesleft;
-    //printf("writing rest of %d frames\n", (int)subframe);
     err64=ambix_writef_float32(ambix, ambidata+fullambichannels*frame*chunksize, otherdata+extrachannels*frame*chunksize, subframe);
     if(fail_if((err64!=subframe), __LINE__, "wrote only %d subframe of %d", (int)err64, (int)subframe))return 1;
 
@@ -111,9 +108,9 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
     if(fail_if((err64!=framesize), __LINE__, "wrote only %d frames of %d", (int)err64, (int)framesize))return 1;
   }
 
-  diff=data_diff(__LINE__, orgambidata, ambidata, framesize*fullambichannels, eps);
+  diff=data_diff(__LINE__, fmt, orgambidata, ambidata, framesize*fullambichannels, eps);
   if(fail_if((diff>eps), __LINE__, "ambidata diff %f > %f", diff, eps))return 1;
-  diff=data_diff(__LINE__, orgotherdata, otherdata, framesize*extrachannels, eps);
+  diff=data_diff(__LINE__, fmt, orgotherdata, otherdata, framesize*extrachannels, eps);
   if(fail_if((diff>eps), __LINE__, "otherdata diff %f > %f", diff, eps))return 1;
 
   if(fail_if((AMBIX_ERR_SUCCESS!=ambix_close(ambix)), __LINE__, "closing ambix file %p", ambix))return 1;
@@ -134,7 +131,6 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
 
   gotframes=0;
   do {
-    //err64=ambix_readf_float32(ambix, resultambidata, resultotherdata, framesize);
       err64=ambix_readf_float32(ambix,
 			resultambidata +(gotframes*fullambichannels ),
 			resultotherdata+(gotframes*extrachannels),
@@ -143,10 +139,10 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
     gotframes+=err64;
   } while(err64>0 && gotframes<framesize);
 
-  diff=data_diff(__LINE__, orgambidata, resultambidata, framesize*fullambichannels, eps);
+  diff=data_diff(__LINE__, fmt, orgambidata, resultambidata, framesize*fullambichannels, eps);
   if(fail_if((diff>eps), __LINE__, "ambidata diff %f > %f", diff, eps))return 1;
 
-  diff=data_diff(__LINE__, orgotherdata, resultotherdata, framesize*extrachannels, eps);
+  diff=data_diff(__LINE__, fmt, orgotherdata, resultotherdata, framesize*extrachannels, eps);
   if(fail_if((diff>eps), __LINE__, "otherdata diff %f > %f", diff, eps))return 1;
 
   if(fail_if((AMBIX_ERR_SUCCESS!=ambix_close(ambix)), __LINE__, "closing ambix file %p", ambix))return 1;
@@ -174,7 +170,6 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
 
   gotframes=0;
   do {
-    //err64=ambix_readf_float32(ambix, resultambidata, resultotherdata, framesize);
       err64=ambix_readf_float32(ambix,
 			resultambidata +(gotframes*ambixchannels ),
 			resultotherdata+(gotframes*extrachannels),
@@ -182,11 +177,7 @@ int check_create_b2e(const char*path, ambix_sampleformat_t format,
     if(fail_if((err64<0), __LINE__, "reading frames failed after %d/%d frames", (int)gotframes, (int)framesize))return 1;
     gotframes+=err64;
   } while(err64>0 && gotframes<framesize);
-#if 0
-  diff=data_diff(__LINE__, orgambidata, resultambidata, framesize*ambixchannels, eps);
-  if(fail_if((diff>eps), __LINE__, "ambidata diff %f > %f", diff, eps))return 1;
-#endif
-  diff=data_diff(__LINE__, orgotherdata, resultotherdata, framesize*extrachannels, eps);
+  diff=data_diff(__LINE__, fmt, orgotherdata, resultotherdata, framesize*extrachannels, eps);
   if(fail_if((diff>eps), __LINE__, "otherdata diff %f > %f", diff, eps))return 1;
 
   if(fail_if((AMBIX_ERR_SUCCESS!=ambix_close(ambix)), __LINE__, "closing ambix file %p", ambix))return 1;
