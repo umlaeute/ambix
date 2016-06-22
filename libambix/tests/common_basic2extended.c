d988c2b5cb990deebca64b2181fb1fc8929125dd
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2016-06-22 16:11:42 +0200

    unique-filenames are now generated in the unittestfile itself
    rather than in the helper-functions

--- libambix/tests/common_basic2extended.c
+++ libambix/tests/common_basic2extended.c
@@ -25,7 +25,7 @@
 
 uint32_t max_u32(uint32_t a, uint32_t b) { return (a>b)?a:b;}
 
-int check_create_b2e(const char*cpath, ambix_sampleformat_t format,
+int check_create_b2e(const char*path, ambix_sampleformat_t format,
 		      ambix_matrix_t*matrix, uint32_t extrachannels,
 		      uint32_t chunksize, ambixtest_presentationformat_t fmt,float32_t eps) {
   uint32_t fullambichannels = matrix?matrix->rows:0;
@@ -40,11 +40,8 @@ int check_create_b2e(const char*cpath, ambix_sampleformat_t format,
   int64_t err64, gotframes;
   float32_t diff=0.;
   ambix_err_t err=0;
-  unsigned int pid=ambixtest_uniquenumber();
-  char path[1024];
   STARTTEST("ambi=[%dx%d],extra=%d, format=%d datafmt=%d\n",
 	    matrix?matrix->rows:-1, matrix?matrix->cols:-1, extrachannels, format, fmt);
-  snprintf(path, 1024, "%s.%d.caf", cpath, pid);
   resultambidata=data_calloc(fmt, max_u32(fullambichannels, ambixchannels)*framesize);
   ambidata=data_calloc(fmt, fullambichannels*framesize);
 
